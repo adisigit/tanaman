d7bce9ec7aba582df0465c6660f67be9b33601cc
sigitadi <adi.sigit13@gmail.com>
2024-05-06 11:13:03 +0700

    [ui] beberapa tampilan

--- resources/views/admin/produk.blade.php
+++ resources/views/admin/produk.blade.php
@@ -1,17 +1,23 @@
 <x-app-layout title="Produk">
-    <h1>produk</h1>
-    @foreach ($produks as $key => $item)
-        <p>{{ $item->nama }}</p>
-        <p>{{ $item->deskripsi }}</p>
-        <p>{{ $item->stok }}</p>
-        <p>{{ $item->harga }}</p>
-        <img src="https://k1-toko-tanaman.s3.ap-southeast-1.amazonaws.com/{{$item->gambar_path}}" alt="">
-        <br>
-        <a href="{{ route('produk.edit', $item->id) }}">edit</a>
-        <form action="{{ route('produk.destroy', $item->id) }}" method="post">
-            @csrf
-            @method('delete')
-            <button class="btn btn-danger" type="submit">delete</button>
-        </form>
-    @endforeach
+    <div class="p-8">
+        <h1>produk</h1>
+        <div class="grid grid-cols-4 gap-4 place-items-center">
+            @foreach ($produks as $key => $item)
+                <div class="shadow-md bg-white rounded-md p-8 ">
+                    <img src="https://k1-toko-tanaman.s3.ap-southeast-1.amazonaws.com/{{ $item->gambar_path }}"
+                        alt="">
+                    <p>{{ $item->nama }}</p>
+                    <p>{{ $item->stok }}</p>
+                    <p>{{ $item->harga }}</p>
+                    <br>
+                    <a href="{{ route('produk.edit', $item->id) }}">edit</a>
+                    <form action="{{ route('produk.destroy', $item->id) }}" method="post">
+                        @csrf
+                        @method('delete')
+                        <button class="btn btn-danger" type="submit">delete</button>
+                    </form>
+                </div>
+            @endforeach
+        </div>
+    </div>
 </x-app-layout>
