e6e3a6c42851cec5aaa331891399b864711e0b54
ameliakufah <ameliakufah04@gmail.com>
2024-05-13 08:58:23 +0700

    views admin

--- resources/views/admin/produk.blade.php
+++ resources/views/admin/produk.blade.php
@@ -1,17 +1,36 @@
 <x-app-layout title="Produk">
-    <h1>produk</h1>
-    @foreach ($produks as $key => $item)
-        <p>{{ $item->nama }}</p>
-        <p>{{ $item->deskripsi }}</p>
-        <p>{{ $item->stok }}</p>
-        <p>{{ $item->harga }}</p>
-        <img src="https://k1-toko-tanaman.s3.ap-southeast-1.amazonaws.com/{{$item->gambar_path}}" alt="">
-        <br>
-        <a href="{{ route('produk.edit', $item->id) }}">edit</a>
-        <form action="{{ route('produk.destroy', $item->id) }}" method="post">
-            @csrf
-            @method('delete')
-            <button class="btn btn-danger" type="submit">delete</button>
-        </form>
-    @endforeach
+    <section class="the-wedding-section section-bg section-padding" id="section_3">
+        <div class="up-center-text">
+            <div class="row">
+                <h2 class="section-title">Produk</h2>
+            </div>
+            
+            <div class="row">
+                @foreach ($produks as $key => $item)
+                <div class="col-lg-4 col-md-6 col-12">
+                    <div class="update-cart">
+                        <div class="cart">
+                            <img src="https://k1-toko-tanaman.s3.ap-southeast-1.amazonaws.com/{{$item->gambar_path}}" class="card-img-top img-thumbnail mx-auto" alt="..." style="width: 200px; height: 200px;">
+                        </div>
+
+                        <div class="card">
+                            <h5 class="card-title">{{ $item->nama }}</h5>
+                            <p class="card-text">{{ $item->deskripsi }}</p>
+                            <p class="card-text">Stok: {{ $item->stok }}</p>
+                            <p class="card-text">Harga: {{ $item->harga }}</p>
+                        </div>
+                        <div class="card-footer">
+                            <a href="{{ route('produk.edit', $item->id) }}" class="btn btn-primary">Edit</a>
+                            <form action="{{ route('produk.destroy', $item->id) }}" method="post" class="d-inline">
+                                @csrf
+                                @method('delete')
+                                <button class="btn btn-danger" type="submit">Delete</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
 </x-app-layout>
--- resources/views/admin/produk_tambah.blade.php
+++ resources/views/admin/produk_tambah.blade.php
@@ -1,22 +1,46 @@
-<x-app-layout title="Tambah">
-    <h1>halaman upload</h1>
+<x-app-layout title="Tambah" class="bg-green-100 flex justify-center items-center">
+    <h1 class="text-green-800 text-2xl mb-6 text-center">Halaman Upload</h1>
     <form action="{{ route('produk.store') }}" method="post" enctype="multipart/form-data">
-        @csrf
-        <div class="gambar">
-            <p>Pilih gambar yang mau di upload:</p> 
-            <img id="uploadPreview" />
-            <br>
-            <input type = "button" value = "Choose image" onclick="javascript:document.getElementById('imgToUpload').click();">
-            <input type="file" name="gambar" id="imgToUpload" onchange="PreviewImage();" style="display: none;" accept="image/jpeg,image/png,image/gif,image/svg+xml">  
+      @csrf
+      <fieldset class="mb-6">
+        <legend class="text-green-800 mb-2">Input gambar yang ingin diupload:</legend>
+        <img id="uploadPreview" class="w-full mb-4">
+        <input type="file" name="gambar" id="imgToUpload" onchange="PreviewImage();" accept="image/jpeg,image/png,image/gif,image/svg+xml" aria-describedby="gambar-help" class="sr-only">
+        <button type="button" class="bg-white text-green-500 hover:bg-green-100 active:bg-green-200 focus:ring-green-500 focus:ring-4 focus:outline-none focus:ring-opacity-50 rounded-lg shadow-sm inline-flex items-center justify-center w-full mb-4 gap-2 px-4 py-2" aria-label="Pilih Gambar" onclick="document.getElementById('imgToUpload').click();">
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 15.75 7.5 12M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0z" />
+  </svg>
+    Pilih Gambar
+</button>
+        <p id="gambar-help" class="text-green-800 text-sm">Format gambar yang dapat diinputkan : JPEG, PNG, GIF, SVG</p>
+      </fieldset>
+      <fieldset class="mb-6">
+        <legend class="text-green-800 mb-2 text-center">Informasi Produk</legend>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div>
+            <label for="nama" class="text-green-800 mb-2">Nama produk</label>
+            <input type="text" name="nama" id="nama" class="w-full border border-green-500 rounded-lg px-4 py-2" aria-label="Nama produk">
+          </div>
+          <div>
+            <label for="deskripsi" class="text-green-800 mb-2">Deskripsi produk</label>
+            <input type="text" name="deskripsi" id="deskripsi" class="w-full border border-green-500 rounded-lg px-4 py-2" aria-label="Deskripsi produk">
+          </div>
+          <div>
+            <label for="stok" class="text-green-800 mb-2">Stok produk</label>
+            <input type="number" name="stok" id="stok" class="w-full border border-green-500 rounded-lg px-4 py-2" aria-label="Stok produk">
+          </div>
+          <div>
+            <label for="harga" class="text-green-800 mb-2">Harga produk</label>
+            <input type="number" name="harga" id="harga" class="w-full border border-green-500 rounded-lg px-4 py-2" aria-label="Harga produk">
+          </div>
         </div>
-        <p>Nama produk</p>
-        <input type="text" name="nama" id="">
-        <p>Deskripsi produk</p>
-        <input type="text" name="deskripsi" id="">
-        <p>Stok produk</p>
-        <input type="number" name="stok" id="">
-        <p>Harga produk</p>
-        <input type="number" name="harga" id="">
-        <input type="submit" value="tambah">
+      </fieldset>
+      <button type="submit" class="bg-white text-green-500 hover:bg-green-100 active:bg-green-200 focus:ring-green-500 focus:ring-4 focus:outline-none focus:ring-opacity-50 rounded-lg shadow-sm inline-flex items-center justify-center w-full mb-4 gap-2 px-4 py-2" aria-label="Tambah produk">
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+  </svg>
+  Tambah
+</button>
     </form>
+  </main>
 </x-app-layout>
